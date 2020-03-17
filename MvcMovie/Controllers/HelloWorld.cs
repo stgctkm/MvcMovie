24c35bae2572df2ae6afedd3060f36e27068d2ab
setoguchi <setoguchi.takuma@gmail.com>
2020-03-17 10:57:36 +0900

    Initial commit

---
+++ MvcMovie/Controllers/HelloWorld.cs
@@ -0,0 +1,23 @@
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        // GET: /HelloWorld/Welcome/ 
+        // Requires using System.Text.Encodings.Web;
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+    }
+}
